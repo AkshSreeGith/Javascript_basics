6bfcafb99fef1ae880e0d0fcb9c54de1fb904a9a
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-04-17 11:28:29 +0530

    Video - 2(datatypes)

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";    // by writing use strict it tells the system use the latest version of javascript
+
+// code readability should be high
+
+console.log(3
+    +3);
+// Datatypes
+
+// bigInt 
+// boolean => (true,false)
+// string => "Akshay"
+// null => standalone value
+// undefined => undefined 
+// symbol => unique
+
+let age =20;
+let name="Akshay"
+let place; // undefined has the type undefined
+let temperature = null; // Null value always dispay the type as object 
+
+console.log(typeof "Akshay");
+console.log(typeof temperature);
+
