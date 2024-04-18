34de3d741c7a147533a0f43d73845ea8e2e82589
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-04-18 22:19:50 +0530

    video - 11(string functions)

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,4 +13,37 @@ console.log(`Hello i am ${name} fuckin ${lastName}`);
 let superHero = new String("Spiderman");
 console.log(superHero);
 superHero = superHero.toLowerCase();
-console.log(superHero);
+console.log(superHero)
+
+const gameName = new String('akshay-ak-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); proto is not recommended to use now in modern javascript 
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   akshay    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+// There are string methods in javascript which can be explored in javascript mdn documents 
