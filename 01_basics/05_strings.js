411d3662322f266ad111004b044e77bde7d5cfee
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-04-18 10:02:57 +0530

    video -11(string)

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+"use strict";
+ 
+const name= "Tyler ";
+const lastName = "Durden";
+
+// This is the old method that is used 
+console.log("Hello my name is "+name+" and my last name is "+lastName);
+
+// new up to date approach 
+console.log(`Hello i am ${name} fuckin ${lastName}`);
+
+
+let superHero = new String("Spiderman");
+console.log(superHero);
+superHero = superHero.toLowerCase();
+console.log(superHero);
