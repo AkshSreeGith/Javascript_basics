5dd4dc3e543b43036394d4aefe623166a8dda3c9
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-04-30 10:52:45 +0530

    video -16,17,18(objects in depth)

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Adding symbol to a object
+const sym = Symbol("key1");
+
+const myObj = {
+  name: "Akshay",
+  age: 20,
+  email: "akshay@gmail.com",
+  isLoggedIn: true,
+  lastLoginDays: ["Monday", "Sunday"],
+  [sym]: "mykey1", //It's important to use [] brackets to use symbol as a key
+};
+
+// Adding function to an object
+myObj.greeting = function () {
+  console.log("Hellow Akshay welcome back!!!!");
+};
+
+// Accesing an object key-value pair
+// First method
+console.log(myObj.email);
+
+// Second method (Correct way)
+console.log(myObj["lastLoginDays"]);
+
+console.log(myObj.greeting());
+
+// Freezing object
+// Object.freeze(myObj);
+
+console.log(myObj[sym]);
