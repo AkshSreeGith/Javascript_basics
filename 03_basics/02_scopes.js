9050c71b1c8bbacee99539952ef2dc4a31b07a5f
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-05-06 16:43:32 +0530

    video -21,22(Scopes)

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -17,3 +17,27 @@ if (true) {
 console.log(a);
 console.log(b);
 console.log(c);
+//closure peoperties
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+// function one(){
+//     const user="pranav"
+//     console.log("Enter")
+//     two()
+//     function two(){
+//         const website = "youtube"
+//         console.log(user)
+//     }
+//     console.log(website)
+
+// }
+// one()
+// **************************************
+// addone(2)//No error
+// function addone(num){//function
+//     return num+1
+// }
+
+// addtwo(4) // Error occur cause compiler do not what addtwo expression or variable is
+// const addtwo = function(num){//expression
+//     return num+2
+// }
