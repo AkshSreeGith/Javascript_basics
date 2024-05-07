85c9cb0757a6df5206a3ac5f84de56dddc4e4c66
AkshSreeGith <akshaysreedhar2003@gmail.com>
2024-05-07 07:20:49 +0530

    video -26(Control flow)

---
+++ 04_control_flow/01_control_flow.js
@@ -0,0 +1,7 @@
+// if else , arithmetic operator,logical operator, && ,||,===,!==,switch case
+// truthy and flasy
+// Nullish Coalescing Operator (??) : null undefined
+//val = 5 ?? 10
+//This is usally used for variable where is 5 is not available than 10 is assigned and vice versa , if first value is null then the value next first will be declared (null ?? 13)
+
+//Terniary Operator - Condition ? true : false
